1e8a2051a595393105c8afa9d5b58f05c35bb281
Ritik-the-dev <joshiritik865@gmail.com>
2026-04-28 08:01:01 +0530

    debugging: silents fails

--- orchestrator.py
+++ orchestrator.py
@@ -12,26 +12,47 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+print("🚀 STARTING PIPELINE")
+
 def run_once():
     try:
+
+        send_telegram("🚀 GitHub pipeline started")
+
+        print("👉 Step 1: Generating video")
+
         logging.info("Starting video generation")
 
         # GEN_PYTHON = "ai-video-generator/venv/Scripts/python.exe"
         # UPLOADER_PYTHON = "ai-video-uploader/upload-env/Scripts/python.exe" 
 
         # 1. Generate video
-        subprocess.run(
+        result = subprocess.run(
             ["python", "run.py"],
             cwd="ai-video-generator",
             check=True,
         )
 
+        print("STDOUT:", result.stdout)
+        print("STDERR:", result.stderr)
+
+        if result.returncode != 0:
+            raise Exception("Generation failed")
+
+        print("👉 Step 2: Uploading video")
+
         # 2. Upload video
-        subprocess.run(
+        result2 = subprocess.run(
             ["python","-m", "src.watch_and_upload",],
             cwd="ai-video-uploader",
             check=True
         )
+        print("STDOUT:", result2.stdout)
+        print("STDERR:", result2.stderr)
+
+        if result2.returncode != 0:
+            raise Exception("Upload failed")
+
         print("Uploaded Properly")
         send_telegram("✅ Video uploaded successfully!")
         logging.info("Upload successful")
